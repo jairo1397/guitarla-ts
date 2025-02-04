b2f559ed3f4d39c2e1af4585b2cf17176dc7a605
Jairo Espinoza <jairo.espinoza.quispe@gmail.com>
2025-02-03 23:31:53 -0500

    migrar a useReducer

--- src/hooks/useCart.ts
+++ src/hooks/useCart.ts
@@ -1,6 +1,5 @@
-import { useState, useEffect, useMemo } from 'react'
-import { db } from '../data/db'
-import type { Guitar, CartItem, GuitarId } from '../types/types'
+import { useState, useEffect } from 'react'
+import type { CartItem, GuitarId } from '../types/types'
 
 export const useCart = () => {
 
@@ -9,7 +8,6 @@ export const useCart = () => {
         return localStorageCart ? JSON.parse(localStorageCart) : []
     }
 
-    const [data] = useState(db)
     const [cart, setCart] = useState(initialCart)
 
     const MIN_ITEMS = 1
@@ -19,18 +17,7 @@ export const useCart = () => {
         localStorage.setItem('cart', JSON.stringify(cart))
     }, [cart])
 
-    function addToCart(item: Guitar) {
-        const itemExists = cart.findIndex(guitar => guitar.id === item.id)
-        if(itemExists >= 0 ) { // existe en el carrito
-            if(cart[itemExists].quantity >= MAX_ITEMS) return
-            const updatedCart = [...cart]
-            updatedCart[itemExists].quantity++
-            setCart(updatedCart)
-        } else {
-            const newItem : CartItem = {...item, quantity: 1}
-            setCart([...cart, newItem])
-        }
-    }
+    
 
     function removeFromCart(id: GuitarId) {
         setCart(prevCart => prevCart.filter(guitar => guitar.id !== id))
@@ -66,19 +53,12 @@ export const useCart = () => {
         setCart([])
     }
 
-    // State Derivado
-    const isEmpty = useMemo( () => cart.length === 0, [cart])
-    const cartTotal = useMemo( () => cart.reduce( (total, item ) => total + (item.quantity * item.price), 0), [cart] )
 
     return {
-        data,
         cart,
-        addToCart,
         removeFromCart,
         decreaseQuantity,
         increaseQuantity,
         clearCart,
-        isEmpty,
-        cartTotal
     }
 }
